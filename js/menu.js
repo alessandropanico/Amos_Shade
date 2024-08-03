6e6ad3d3c870b2114b18424c21b14dfba7cda894
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-08-03 17:17:17 +0200

    asd

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,40 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+
+let startX;
+
+function openNav() {
+    document.getElementById("mySidenav").classList.add("open");
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").classList.remove("open");
+}
+
+function handleTouchStart(event) {
+    startX = event.touches[0].clientX;
+}
+
+function handleTouchMove(event) {
+    const currentX = event.touches[0].clientX;
+    const diffX = currentX - startX;
+
+    if (diffX > 50) {
+        openNav();
+    } else if (diffX < -50) {
+        closeNav();
+    }
+}
+
+function handleTouchEnd(event) {
+    startX = null;
+}
+
+document.addEventListener("touchstart", handleTouchStart);
+document.addEventListener("touchmove", handleTouchMove);
+document.addEventListener("touchend", handleTouchEnd);
+
+document.querySelector(".menu-btn").addEventListener("click", openNav);
+document.querySelector(".closebtn").addEventListener("click", closeNav);
